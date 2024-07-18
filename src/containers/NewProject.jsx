3dff565a4ba6c57bf78367e428c8baa77af1034a
anand9773 <aaanand9773@gmail.com>
2024-07-18 08:56:14 +0530

    code Editor section completed

--- src/containers/NewProject.jsx
+++ src/containers/NewProject.jsx
@@ -5,12 +5,26 @@ import SplitPane from "react-split-pane";
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 import "./styles.css"; // Import the custom scrollbar styles
+import { Link, useAsyncError } from "react-router-dom";
+import logo from "../assets/logo_s.svg"; // Import the SVG file
+import { AnimatePresence, motion } from "framer-motion";
+import { MdCheck, MdEdit } from "react-icons/md";
+import { useSelector } from "react-redux";
+import { UserProfileDetails } from "../components";
+import { doc, setDoc } from "firebase/firestore";
+import Alert from "../components/Alert/Alert";
+import { db } from "../config/Firebase.config";
 
 const NewProject = () => {
   const [html, setHtml] = useState("");
   const [css, setCss] = useState("");
   const [js, setjs] = useState("");
   const [output, setOutput] = useState("");
+  const [title, setTitle] = useState("Untitled");
+  const [isTitle, setIsTitle] = useState(false);
+  const [alert, setAlert] = useState(false);
+
+  const user = useSelector((state) => state.user.user);
 
   useEffect(() => {
     updateOutput();
@@ -30,12 +44,121 @@ const NewProject = () => {
     `;
     setOutput(combinedOutput);
   };
+
+  const saveProgram = async () => {
+    const id = `${Date.now()}`;
+    const _doc = {
+      id: id,
+      user: user,
+      title: title,
+      html: html,
+      css: css,
+      js: js,
+      output: output,
+    };
+    await setDoc(doc(db, "Projects", id), _doc)
+      .then((res) => {
+        setAlert(true);
+      })
+      .catch((error) => console.log(error));
+
+    setInterval(() => {
+      setAlert(false);
+    }, 2000);
+  };
   return (
     <div className="w-screen h-screen flex flex-col items-start justify-start overflow-hidden">
       {/* alert area       */}
+      {alert && <Alert status={"Success"} alertMsg={"Project Saved...."} />}
 
       {/* header area */}
-      <header className="w-full flex items-center"></header>
+      <header className="w-full flex items-center justify-between px-8 py-2">
+        <div className="flex items-center justify-between px-2 py-2">
+          <div className="flex items-center justify-center gap-3 ">
+            <Link to={"/home/projects"}>
+              <img
+                src="https://imgs.search.brave.com/3sbhxnL9e6esNB6Ey0nouI1TEcNIbqNl17v8b_GUDyw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMuc3RpY2twbmcu/Y29tL2ltYWdlcy81/ODQ3ZWJiMWNlZjEw/MTRjMGI1ZTQ4NTUu/cG5n"
+                className="w-12 h-auto object-contain"
+              />
+            </Link>
+            <div className="flex flex-col items-start justify-start">
+              <div className="flex items-center justify-center gap-3">
+                <AnimatePresence>
+                  {isTitle ? (
+                    <>
+                      <motion.input
+                        key={"TitleInput"}
+                        type="text"
+                        placeholder="Your Title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        className="px-3 py-1 rounded-md text-primaryText text-base outline-none border-none bg-transparent"
+                      />
+                    </>
+                  ) : (
+                    <>
+                      {" "}
+                      <motion.p
+                        key={"titleLabel"}
+                        className="px-3 py-2 text-white text-lg"
+                      >
+                        {title}
+                      </motion.p>
+                    </>
+                  )}
+                </AnimatePresence>
+                <AnimatePresence>
+                  {isTitle ? (
+                    <motion.div
+                      key={"MdCheck"}
+                      whileTap={{ scale: 0.9 }}
+                      className="cursor-pointer"
+                      onClick={() => setIsTitle(false)}
+                    >
+                      <MdCheck className="text-2xl text-emerald-500" />
+                    </motion.div>
+                  ) : (
+                    <motion.div
+                      key={"MdEdit"}
+                      whileTap={{ scale: 0.9 }}
+                      className="cursor-pointer"
+                      onClick={() => setIsTitle(true)}
+                    >
+                      <MdEdit className="text-2xl text-primaryText" />
+                    </motion.div>
+                  )}
+                </AnimatePresence>
+              </div>
+              {/* follow section */}
+              <div className="flex items-center justify-center px-3 -mt-2 gap-2">
+                <p className="text-primaryText text-sm">
+                  {user?.displayName
+                    ? user?.displayName
+                    : `${user?.email.split("@")[0]}`}
+                </p>
+                <motion.p
+                  whileTap={{ scale: 0.9 }}
+                  className="text-[10px] font-semibold bg-emerald-500 rounded-sm px-2 py-[1px] text-primary  cursor-pointer outline-none"
+                >
+                  follow
+                </motion.p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* user section */}
+        {user && (
+          <div className="flex items-center justify-center gap-4">
+            <motion.button
+              onClick={saveProgram}
+              className="px-6 py-4 bg-primaryText cursor-pointer text-base text-primary font-semibold  rounded-md"
+            >
+              Save
+            </motion.button>
+            <UserProfileDetails />
+          </div>
+        )}
+      </header>
       {/* coding area */}
       <div>
         {/* horizontal */}
@@ -53,7 +176,7 @@ const NewProject = () => {
                 <div className="bg-secondary px-4 py-2 border-t-4 flex itmes-center justify-center gap-3 border-t-gray-500 ">
                   <FaHtml5 className="text-xl text-red-500" />
 
-                  <p className="text-primaryText font-semibold ">HMTL</p>
+                  <p className="text-primaryText font-semibold ">HTML</p>
                 </div>
                 <div className="cursor-pointer flex items-center justify-center gap-5 px-4">
                   <FcSettings className="text-xl text-primaryText" />
